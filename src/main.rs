b6facf05b556ad0ee91d8f1ea47c32240813db8e
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-10 14:18:51 +0300

    slice array example

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 fn main() { 
    let a = [1,2,3,4,5];
    
-   let slice = &a[1..];
+   let slice = &a[2..4];
 
    println!("the sliced array has the following: {:?}", slice); //normal print for array
    println!("the sliced array has the following: {:#?}", slice); //pretty print
