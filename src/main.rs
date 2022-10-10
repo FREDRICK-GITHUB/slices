5e8ecf984fbde4df769135ad7e43f7aa1dc015f5
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-10 12:57:22 +0300

    initial commit using len for slice

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@
 fn main() {
     let s = String::from("hello world!");
 
-    let hello = &s[0..5]; //slice string up to the fourth index
-    let world = &s[6..12]; //slice string from the fifth to the eleventh index
-    let all = &world[..];  //slice the string from the first to the last index
+    let len = s.len();
 
-    println!("{}", hello);
-    println!("{}", world);
-    println!("{}", all);
+    let slice = &s[3..len];
+    let slice = &s[..len];
+
+    println!("{}", len);
+    println!("{}", slice);
+    println!("{}", slice);
+    
 }
