4f53e6fbf54423d6e96e84affb660000142113fa
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-10 13:11:32 +0300

    example to slice a word from a string when first space is encountered

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 
-fn main() {
-    let s = String::from("hello world!");
+fn main() { // this is a simple exampe to split a string and return first word in string
+    let ss = String::from("hello world!");
 
-    let len = s.len();
+    let first_word = first_word(&ss);
 
-    let slice = &s[3..len];
-    let slice = &s[..len];
-
-    println!("{}", len);
-    println!("{}", slice);
-    println!("{}", slice);
+    println!("the first word in the above string is: {}",first_word);
     
+}
+
+fn first_word(s: &String) -> &str {  // this function takes a reference and returns a reference (borrower)
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() { //iterate through the bytes and add them into a tuple
+        if item == b' ' { //if item at the index reference is a space
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
