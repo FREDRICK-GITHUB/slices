9a238029be92bfb8d01b092f4d2027cff1721645
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-10 14:18:24 +0300

    slice array example

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,10 @@
-/*In this example, we changed the function parameter from String reference (s: $String) 
-to using the slice for the string (s: *str) which is the data type for string literals as well*/
-fn main() { // this is a simple exampe to split a string and return first word in string
-    let my_string = String::from("hello world!");
 
-    let word = first_word(&my_string[..]);
-    println!("the word is: {}",word);
+fn main() { 
+   let a = [1,2,3,4,5];
+   
+   let slice = &a[1..];
 
-    let my_string_literal = "hello world!";
-    let word = first_word(&my_string_literal[..]);
-    println!("the word is: {}",word);
-
-    let word = first_word(my_string_literal);
-    println!("the word is: {}",word);
-
-    println!("the second word is: {}", second_word(my_string_literal));
-    
+   println!("the sliced array has the following: {:?}", slice); //normal print for array
+   println!("the sliced array has the following: {:#?}", slice); //pretty print
 }
 
-fn first_word(s: &str) -> &str {  // this function takes a reference and returns a reference (borrower)
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() { //iterate through the bytes and add them into a tuple
-        if item == b' ' { //if item at the index reference is a space
-            return &s[0..i];
-        }
-    }
-
-    &s[..]
-}
-
-fn second_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[i..];
-        }
-    }
-
-    &s[..]
-}
