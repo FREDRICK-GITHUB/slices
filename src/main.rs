06b5e8f764e70b756e5ae2850c3677a58f864b5f
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-01-21 15:48:40 +0300

    string and string slice notes added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 
 fn main() { 
-   let a = [1,2,3,4,5];
-   
-   let slice = &a[2..4];
+   //This is a string. Stored on heap because size can expand or shrink during runtime
+   let string_one = String::from("127.0.0.1:8080");
 
-   println!("the sliced array has the following: {:?}", slice); //normal print for array
-   println!("the sliced array has the following: {:#?}", slice); //pretty print
+   //unofficial way of showing the slice from the first bit, return three bits
+   let string_slice = &string_one[0..3];
+
+   //borrowing a String as a string slice
+   let borrowed_string: &str = &string_one;
+   
+   //string literal is treated as a string slice since size is known at compile time
+   let string_literal = "1234";
+
+   //find() returns the string slice at a given bit
+   let indexer = &string_literal.find("2");
+
+   dbg!(&string_one);
+   dbg!(string_slice);
+   dbg!(borrowed_string);
+   dbg!(string_literal);
+   dbg!(indexer);
+   println!("slice values: {} from string: {}", string_slice, string_one);
 }
 
